4ad21a6010d9a0f0323aa3b459996808af89a49f
ElicitWeb <144059043+ElicitWeb@users.noreply.github.com>
2023-09-07 18:51:16 +0530

    Add files via upload

---
+++ Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './title.css';
+import "@fontsource/readex-pro";
+
+const Title =()=>{
+    return(
+        <div className="heading-box">
+			<div className="heading">Elicit'22 <span className="heading2">Events</span></div>
+		</div>
+    )
+}
+
+export default Title;
